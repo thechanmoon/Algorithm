f010bd8c487b9a778ef772dd4bc6457c5e55c0be
chan <thechanmoon@gmail.com>
2020-10-11 23:04:17 -0400

    Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,46 @@
+class BubbleSort {
+    int[] array;
+
+    public BubbleSort(int[] array) {
+        this.array = array;
+    }
+
+    public int[] sort() {
+        return sort(array.length);
+    }
+
+    public int[] sort(int last) {
+        if (last > 0) {
+            for (int i = 1; i < last; i++) {
+                if (array[i] < array[i - 1]) {
+                    swap(i, i - 1);
+                }
+            }
+            sort(last - 1);
+        }
+        return array;
+    }
+
+    public void swap(int i1, int i2) {
+        array[i1] = array[i1] + array[i2];
+        array[i2] = array[i1] - array[i2];
+        array[i1] = array[i1] - array[i2];
+    }
+
+    static int[] sort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+
+            for (int j = 0; j < a.length - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    // swap(a[j], a[j + 1]);
+                    a[j] = a[j] + a[j + 1];
+                    a[j + 1] = a[j] - a[j + 1];
+                    a[j] = a[j] - a[j + 1];
+                }
+            }
+
+        }
+        return a;
+    }
+}
--- Test.java
+++ Test.java
@@ -2,15 +2,28 @@ import java.util.*;
 
 class Test {
     public static void main(String[] args) {
-        boolean[][] grid = { { false, false, false, false }, { false, false, false, true },
-                { true, true, false, false }, { false, false, false, false } };
-        Robot robot = new Robot();
-        ArrayList<Point> path = robot.findPath(grid);
-        if (path != null) {
-            for (Point p : path) {
-                System.out.print(p.row + "" + p.col + " -> ");
-            }
-        }
+        // boolean[][] grid = { { false, false, false, false }, { false, false, false,
+        // true },
+        // { true, true, false, false }, { false, false, false, false } };
+        // Robot robot = new Robot();
+        // ArrayList<Point> path = robot.findPath(grid);
+        // if (path != null) {
+        // for (Point p : path) {
+        // System.out.print(p.row + "" + p.col + " -> ");
+        // }
+        // }
         // System.out.println("Test");
+        int[] arr = { 3, 5, 4, 2, 1 };
+
+        // BubbleSort bs = new BubbleSort(arr);
+        // int[] temp = bs.sort();
+
+        // for (int n : temp) {
+        // System.out.println(n);
+        // }
+
+        for (int n : BubbleSort.sort(arr)) {
+            System.out.println(n);
+        }
     }
 }
