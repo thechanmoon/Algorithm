a926b380c84c40a835e93ddd46fd1d41b5946a96
chan <thechanmoon@gmail.com>
2020-11-21 17:33:08 -0500

    leetcode 458,1295 array Make it better

--- leetcode/1295/findNumbers.js
+++ leetcode/1295/findNumbers.js
@@ -1,7 +1,8 @@
 var findNumbers = function(nums) {
     let count = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (isEvenNumberDigits(nums[i])) {
+        // if (isEvenNumberDigits(nums[i])) {
+        if (nums[i].toString().length % 2 === 0) {
             count++;
         }
     }
