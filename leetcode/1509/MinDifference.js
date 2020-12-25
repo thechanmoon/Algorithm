ae680de7fd2c69009e30cda34c983d3f51a2d6bc
chan <thechanmoon@gmail.com>
2020-12-25 15:54:31 -0500

    leetcode 1509 console.log test

--- leetcode/1509/MinDifference.js
+++ leetcode/1509/MinDifference.js
@@ -3,9 +3,15 @@ let minDifference = (nums) => {
     let min = Number.MAX_SAFE_INTEGER;
     
     nums.sort((a, b) => a - b);
+    console.log(nums);
     for(let i = 0; i<=3; i++){
+        console.log(nums.length-1-i,3-i);
+        console.log(nums[nums.length-1-i],nums[3-i]);
+        console.log('=========');
         min = Math.min(min,nums[nums.length-1-i] - nums[3-i]);
     }
     
     return min;
 };
+
+console.log(minDifference([1, 5, 0, 10, 14]));
