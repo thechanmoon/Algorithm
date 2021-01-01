3e626be7c5ecb16d3ec7c8c9a1d57545a1445736
chan <thechanmoon@gmail.com>
2021-01-01 15:18:08 -0500

    leetcode 122 c#

---
+++ leetcode/122/MaxProfit.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        if (prices.Length == 0)
+            return 0;
+
+        int profit = 0;
+
+        for (int i = 0; i < prices.Length - 1; i++)
+        {
+            if (prices[i + 1] > prices[i])
+            {
+                profit += prices[i + 1] - prices[i];
+            }
+        }
+
+        return profit;
+    }
+}
