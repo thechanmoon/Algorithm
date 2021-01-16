2ee88d0193f5a0ee65b33dd95244308d4afce4b7
chan <thechanmoon@gmail.com>
2021-01-16 00:08:13 -0500

    leetcode 1170 C# make it better

--- leetcode/1170/SmallerFrequency.cs
+++ leetcode/1170/SmallerFrequency.cs
@@ -1,30 +1,93 @@
-public static int[] NumSmallerByFrequency(string[] queries, string[] words)
+public class Solution
 {
-    int[] result = new int[queries.Length];
-    int[] arr = new int[words.Length];
-    for (int i = 0; i < words.Length; i++)
-        arr[i] = f(words[i]);
-
-    for (int i = 0; i < queries.Length; i++)
+    public int[] NumSmallerByFrequency(string[] queries, string[] words)
     {
-        int ans = f(queries[i]);
-        result[i] = arr.Count(num => ans < num);
-    }
+        int[] result = new int[queries.Length];
 
-    return result;
 
-    int f(string s)
-    {
-        var dic = new Dictionary<char, int>();
-        char smallest = 'z';
-        foreach (var ch in s)
+        int[] query = new int[queries.Length];
+        int[] word = new int[words.Length];
+
+        for (int i = 0; i < queries.Length; i++)
         {
-            smallest = smallest > ch ? ch : smallest;
-            if (dic.ContainsKey(ch))
-                dic[ch]++;
-            else dic.Add(ch, 1);
+            query[i] = getMinQuery(queries[i]);
         }
 
-        return dic[smallest];
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            word[i] = getMinQuery(words[i]);
+        }
+
+
+        for (int i = 0; i < query.Length; i++)
+        {
+
+            int count = 0;
+            for (int j = 0; j < word.Length; j++)
+            {
+
+                if (query[i] < word[j])
+                {
+                    count++;
+                }
+            }
+
+            result[i] = count;
+        }
+
+        return result;
+    }
+
+    public int getMinQuery(String s)
+    {
+
+        char[] charAr = s.ToCharArray(0, s.Length);
+        Array.Sort(charAr);
+
+        int minLenQuery = 1;
+
+        for (int i = 1; i < charAr.Length; i++)
+        {
+            if (charAr[0] == charAr[i])
+            {
+                minLenQuery++;
+            }
+
+        }
+        return minLenQuery;
+    }
+}
+public class Solution1
+{
+    public static int[] NumSmallerByFrequency(string[] queries, string[] words)
+    {
+        int[] result = new int[queries.Length];
+        int[] arr = new int[words.Length];
+        for (int i = 0; i < words.Length; i++)
+            arr[i] = f(words[i]);
+
+        for (int i = 0; i < queries.Length; i++)
+        {
+            int ans = f(queries[i]);
+            result[i] = arr.Count(num => ans < num);
+        }
+
+        return result;
+
+        int f(string s)
+        {
+            var dic = new Dictionary<char, int>();
+            char smallest = 'z';
+            foreach (var ch in s)
+            {
+                smallest = smallest > ch ? ch : smallest;
+                if (dic.ContainsKey(ch))
+                    dic[ch]++;
+                else dic.Add(ch, 1);
+            }
+
+            return dic[smallest];
+        }
     }
 }
