7cca186de64cec75e866964a6df6eb9fdb585e66
chan <thechanmoon@gmail.com>
2021-01-24 18:41:09 -0500

    leecode  1647 debug

--- leetcode/1647/MinDeletions.java
+++ leetcode/1647/MinDeletions.java
@@ -17,16 +17,28 @@ class Solution {
         Arrays.sort(freq);
         int exp = freq[25];
         int res = 0;
+
+        System.out.println(Arrays.toString(freq));
+
         for (int i = 25; i >= 0; i--) {
             if (freq[i] == 0)
                 break;
             if (freq[i] > exp) {
+                System.out.println("1 i : " + i);
+                System.out.println("2 freq[i] : " + freq[i]);
+                System.out.println("3 exp : " + exp);
+                System.out.println("4 res : " + res);
                 res += freq[i] - exp;
             } else {
+                System.out.println("5 freq[i] : " + freq[i]);
+                System.out.println("6 exp : " + exp);
                 exp = freq[i];
             }
-            if (exp > 0)
+            if (exp > 0) {
+                System.out.println("7 exp : " + exp);
                 exp--; // Lowest exp is zero, cannot be negative
+            }
+            System.out.println("========");
         }
         return res;
     }
