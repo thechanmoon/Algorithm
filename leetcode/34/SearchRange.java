243b0412b61d023e405dd9ef74958aabdf656b5d
chan <thechanmoon@gmail.com>
2021-02-19 21:18:19 -0500

    leetcode 34

--- leetcode/34/SearchRange.java
+++ leetcode/34/SearchRange.java
@@ -1,5 +1,45 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        return new int[] { findFirst(nums, target), findLast(nums, target) };
+    }
 
+    public int findFirst(int[] nums, int target) {
+        int index = -1;
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] >= target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+            if (nums[mid] == target)
+                index = mid;
+        }
+
+        return index;
+    }
+
+    public int findLast(int[] nums, int target) {
+        int index = -1;
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            if (nums[mid] == target)
+                index = mid;
+        }
+
+        return index;
     }
 }
