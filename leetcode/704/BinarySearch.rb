3bd63750f75219d7f86bc655474b412a5290a7d5
chan <thechanmoon@gmail.com>
2021-02-27 09:21:30 -0500

    leetcode 704 BinarySearch ruby another solution

--- leetcode/704/BinarySearch.rb
+++ leetcode/704/BinarySearch.rb
@@ -2,17 +2,25 @@ def search(nums, target)
     left = 0
     right = nums.size - 1
     while left <= right
-      mid = left + (right-left)/2
-      
-      if nums[mid] == target
+        mid = left + (right-left)/2
+        
+        if nums[mid] == target
         return mid
-      elsif nums[mid] > target
+        elsif nums[mid] > target
         right = mid - 1
-      else
+        else
         left = mid + 1
-      end
+        end
     end
-    return -1
-  end
+return -1
+end
 
-  puts(search([-1,0,3,5,9,12],9));
+def search1(nums, target)
+    if nums.include?target
+        return nums.index(target)
+    end     
+return -1
+end    
+
+puts(search([-1,0,3,5,9,12],9));
+puts(search1([-1,0,3,5,9,12],9));
