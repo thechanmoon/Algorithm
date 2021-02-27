0556fa6735ccfcc6600addcfb7bad508f5a1b475
chan <thechanmoon@gmail.com>
2021-02-27 09:14:01 -0500

    leetcode 704 BinarySearch ruby

---
+++ leetcode/704/BinarySearch.rb
@@ -0,0 +1,18 @@
+def search(nums, target)
+    left = 0
+    right = nums.size - 1
+    while left <= right
+      mid = left + (right-left)/2
+      
+      if nums[mid] == target
+        return mid
+      elsif nums[mid] > target
+        right = mid - 1
+      else
+        left = mid + 1
+      end
+    end
+    return -1
+  end
+
+  puts(search([-1,0,3,5,9,12],9));
