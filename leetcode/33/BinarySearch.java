03bd0136af4f31cafcb503b45a7e9897aa611d6a
Chan Park <thechanmoon@gmail.com>
2021-02-28 22:03:30 -0600

    leetcode 33 Search in Rotated Sorted Array java Debug

--- leetcode/33/BinarySearch.java
+++ leetcode/33/BinarySearch.java
@@ -12,20 +12,66 @@ class Solution {
             if (nums[mid] == target) 
                 return mid;
 
+            System.out.println("left : " +  left);
+            System.out.println("mid : " +  mid);
+            System.out.println("right : " +  right);
+            System.out.println("nums[left] : " +  nums[left]);
+            System.out.println("nums[mid] : " +  nums[mid]);
+            System.out.println("nums[right] : " +  nums[right]);
+            
+
             if (nums[left] <= nums[mid]) {// Left part
+                System.out.println("Debug 0");
                 if (target >= nums[left] && target < nums[mid]) 
+                {
                     right = mid - 1;
+                    System.out.println("Debug 1");
+                }
                 else
+                {
                     left = mid + 1;
+                    System.out.println("Debug 2");
+                }
             }
 
             if (nums[mid] <= nums[right]) {// Right part
+                System.out.println("Debug 3");
                 if (target > nums[mid] && target <= nums[right])
+                {
                     left = mid + 1;
+                    System.out.println("Debug 4");
+                }
                 else
+                {
                     right = mid - 1;
+                    System.out.println("Debug 5");
+                }
             }
+
+            System.out.println("=============================");
         }		
         return -1;
     }
 }
+
+/*
+left : 0
+mid : 3
+right : 7
+nums[left] : 4
+nums[mid] : 7
+nums[right] : 3
+Debug 0
+Debug 2
+=============================
+left : 4
+mid : 5
+right : 7
+nums[left] : 0
+nums[mid] : 1
+nums[right] : 3
+Debug 0
+Debug 1
+=============================
+4
+*/
