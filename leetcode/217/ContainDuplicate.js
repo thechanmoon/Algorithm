fd2b63f3502048d2e2660af3a9c61ce10cbb4766
chan <thechanmoon@gmail.com>
2021-03-12 21:22:31 -0500

    leetcode 217 javacript

---
+++ leetcode/217/ContainDuplicate.js
@@ -0,0 +1,15 @@
+var containsDuplicate = function(nums) {
+        
+    let set = new Set();
+
+    for(let i = 0; i < nums.length; i++)
+    {
+        if(set.has(nums[i]))
+            return true;
+        else
+            set.add(nums[i]);
+    }
+    return false;
+};
+
+console.log(containsDuplicate([1,2,3,1]));
