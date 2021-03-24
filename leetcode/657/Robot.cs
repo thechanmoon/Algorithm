861834cc3e618d96f0ee50c24eddfba2f21c549e
Chan Park <thechanmoon@gmail.com>
2021-03-24 13:36:04 -0500

    leetcode 657 csharp

---
+++ leetcode/657/Robot.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public bool JudgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+        
+        char[] array = moves.ToCharArray();
+        
+        for(int i = 0; i < array.Length; i++)
+        {
+            if(array[i] == 'R')
+                x++;    
+            else if(array[i] == 'L')
+                x--;
+            else if(array[i] == 'U')
+                y++;    
+            else if(array[i] == 'D')
+                y--;
+        }
+        return (x == 0 && y == 0);
+    }
+}
