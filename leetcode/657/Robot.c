9fa72d09dc00bc04e979b2b37c34d735530474b6
Chan Park <thechanmoon@gmail.com>
2021-03-24 14:02:27 -0500

    leetcode 657 c

---
+++ leetcode/657/Robot.c
@@ -0,0 +1,21 @@
+bool judgeCircle(char * moves){
+    
+    int length = strlen(moves);
+    
+    int x = 0;
+    int y = 0;
+    int i = 0;
+    
+    for(i = 0; i < length; i++)
+    {
+        if(moves[i] == 'R')
+            x++;    
+        else if(moves[i] == 'L')
+            x--;
+        else if(moves[i] == 'U')
+            y++;    
+        else if(moves[i] == 'D')
+            y--;
+    }
+    return (x == 0 && y == 0);
+}
