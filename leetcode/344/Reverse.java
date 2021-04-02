cb1a23d19150e0f4b1b58d2611e3c3c7a5bc10a5
chan <thechanmoon@gmail.com>
2021-04-02 09:16:21 -0400

    leecode 344 java make it better

--- leetcode/344/Reverse.java
+++ leetcode/344/Reverse.java
@@ -1,3 +1,16 @@
+import java.util.Arrays;
+import java.util.*;
+
+class Reverse{
+    public static void main(String[] args) {
+        char[] array = {'h','e','l','l','o'};
+        new Solution().reverseString(array);
+        System.out.println(Arrays.toString(array));
+        new Solution1().reverseString(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
+
 class Solution {
     public void reverseString(char[] s) {
         char temp;
@@ -6,6 +19,23 @@ class Solution {
             temp = s[s.length-1-i]; 
             s[s.length-1-i] = s[i];
             s[i] = temp;
+        }
+    }
+}
+
+class Solution1 {
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length-1;
+        char t;
+        
+        while( i < j)
+        {
+            t = s[i];
+            s[i] = s[j];
+            s[j] = t;
+            i++;
+            j--;
         }
     }
 }
