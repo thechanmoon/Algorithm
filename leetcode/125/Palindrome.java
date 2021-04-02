22fe9bf1c380e77dee3b78421b7eb82a756350f3
chan <thechanmoon@gmail.com>
2021-04-02 16:41:22 -0400

    leetcode 125 java reg

--- leetcode/125/Palindrome.java
+++ leetcode/125/Palindrome.java
@@ -1,6 +1,7 @@
 class Palindrome{
     public static void main(String[] args) {
         System.out.println(new Solution().isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(new Solution1().isPalindrome("A man, a plan, a canal: Panama"));
     }
 }
 class Solution {
@@ -20,6 +21,22 @@ class Solution {
         for(int i = 0; i < str.length()/2; i++)
         {
             if(str.charAt(str.length()-1-i) !=  str.charAt(i))
+                return false;
+        }
+        
+        return true;
+    }
+}
+
+class Solution1{
+    public boolean isPalindrome(String s) {
+
+                
+        s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        
+        for(int i = 0; i < s.length()/2; i++)
+        {
+            if(s.charAt(s.length()-1-i) !=  s.charAt(i))
                 return false;
         }
         
