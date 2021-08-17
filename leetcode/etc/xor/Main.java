4c25c39e8501b23ed8c574549ef93572975d6661
chad <thechanmoon@gmail.com>
2021-08-17 14:33:41 -0500

    leetcode 136 java xor

---
+++ leetcode/etc/xor/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a^b);      
+    }
+}
+// [출처] [Java] 코드업 1062 : [기초-비트단위논리연산] 비트단위로 XOR 하여 출력하기|작성자 치즈감자
