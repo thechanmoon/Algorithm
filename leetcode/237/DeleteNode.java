48a4645b3d66c69a18cdbf71b5238892dfb6da7f
chad <thechanmoon@gmail.com>
2021-08-17 18:10:51 -0500

    leetcode 237 java main

--- leetcode/237/DeleteNode.java
+++ leetcode/237/DeleteNode.java
@@ -1,6 +1,6 @@
 
 //  Definition for singly-linked list.
-public class ListNode {
+class ListNode {
     int val;
     ListNode next;
     ListNode(int x) { val = x; }
@@ -13,3 +13,26 @@ class Solution {
     }
 }
 
+class DeleteNode{
+    public static void main(String[] args) {
+        ListNode node0 = new ListNode(4);
+        ListNode node1 = new ListNode(5);
+        ListNode node2 = new ListNode(1);
+        ListNode node3 = new ListNode(9);    
+
+        node0.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        
+        Solution sl = new Solution();
+        sl.deleteNode(node1);
+
+        ListNode head = node0;
+
+        while(head != null)
+        {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+}
