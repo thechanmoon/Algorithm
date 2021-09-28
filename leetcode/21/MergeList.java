d09acda7619363378240e490087bc0dc8f4231f0
chad <thechanmoon@gmail.com>
2021-09-28 18:47:48 -0500

    leetcode 21 java

--- leetcode/21/MergeList.java
+++ leetcode/21/MergeList.java
@@ -47,3 +47,50 @@ class Solution {
         return head;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution1 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        
+        while((l1 != null) || (l2 != null) ){
+            
+            if(l1 == null)
+            {
+                current.next = new ListNode(l2.val);
+                current = current.next;
+                l2 = l2.next;
+            }
+            else if(l2 == null)   
+            {
+                current.next = new ListNode(l1.val);
+                current = current.next;
+                l1 = l1.next;
+            }else{
+                if(l1.val < l2.val)
+                {
+                    current.next = new ListNode(l1.val);
+                    current = current.next;
+                    l1 = l1.next;
+                }else
+                {
+                    current.next = new ListNode(l2.val);
+                    current = current.next;
+                    l2 = l2.next;
+                }      
+            }    
+        }
+        return dummyHead.next;
+    }
+}
