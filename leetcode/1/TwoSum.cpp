1c200d9ee7310bd5eefce1ebd3c033c46fd3261b
chad <thechanmoon@gmail.com>
2021-10-05 17:56:01 -0500

    leecode 1 cpp

---
+++ leetcode/1/TwoSum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int , int> map;
+        for(int i = 0; i < nums.size();i++)
+        {
+            if (map.find(target - nums[i]) != map.end())
+                return {map[target-nums[i]],i};
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
