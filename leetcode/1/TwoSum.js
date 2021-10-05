ef09651a0e585793e4813722883326c222696478
chad <thechanmoon@gmail.com>
2021-10-05 15:48:19 -0500

    leecode 1 javascript

--- leetcode/1/TwoSum.js
+++ leetcode/1/TwoSum.js
@@ -9,6 +9,17 @@ var twoSum = function(nums, target) {
     }
     return [];
 };
+
+const twoSum1 = (numbers, sum) => {
+    const dataObject = {}
+    for(let i =0; i< numbers.length; i++){
+        if(dataObject[sum - numbers[i]] != null){ // 9 & 1
+            return [dataObject[(sum - numbers[i])],i];
+        }
+        dataObject[numbers[i]] = i // 1: 1 
+    }
+}
+
 const twoSum2 = (numbers, sum) => {
     const dataObject = {}
     for(let i =0; i< numbers.length; i++){
@@ -39,10 +50,32 @@ const twoSum2 = (numbers, sum) => {
 
 }
 
+
+var twoSum3 = function(nums, target) {
+    let map = new Map();
+    
+    for(let i = 0; i < nums.length; i++)
+    {
+        if(map.has(target-nums[i]))
+            return [map.get(target-nums[i]),i];
+        map.set(nums[i],i);
+    }
+        
+    return [];
+};
+
 console.log(twoSum([2,7,11,15],9));
 console.log(twoSum([3,2,4],6));
 console.log(twoSum([3,3],6));
-
+console.log();
+console.log(twoSum1([2,7,11,15],9));
+console.log(twoSum1([3,2,4],6));
+console.log(twoSum1([3,3],6));
+console.log();
 console.log(twoSum2([2,7,11,15],9));
 console.log(twoSum2([3,2,4],6));
 console.log(twoSum2([3,3],6));
+console.log();
+console.log(twoSum3([2,7,11,15],9));
+console.log(twoSum3([3,2,4],6));
+console.log(twoSum3([3,3],6));
