537eadb2a3285722a49b3f0693e5ae433594fb83
Chan Park <thechanmoon@gmail.com>
2021-10-07 21:07:06 +0000

    leetcode 121 ruby

--- leetcode/121/MaxProfit.rb
+++ leetcode/121/MaxProfit.rb
@@ -1,6 +1,6 @@
 # @param {Integer[]} prices
 # @return {Integer}
-def max_profit(prices)
+# def max_profit(prices)
     #     if prices.length==0  
     #         return 0;
     #     end
@@ -22,20 +22,36 @@ def max_profit(prices)
     #     maxprofit; 
         
         
-        return 0 if prices.empty?
+    #     return 0 if prices.empty?
         
-        min_price = prices[0]
-        max_profit = 0
+    #     min_price = prices[0]
+    #     max_profit = 0
     
         
-        for i in (0..prices.length - 1)
-            min_price = [min_price, prices[i]].min
-            max_profit = [max_profit, prices[i] - min_price].max
-        end
+    #     for i in (0..prices.length - 1)
+    #         min_price = [min_price, prices[i]].min
+    #         max_profit = [max_profit, prices[i] - min_price].max
+    #     end
         
-        return max_profit
+    #     return max_profit
+        
+    # end
+
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)  
+    min = prices[0];
+    max = 0;
+
+    for i in 1..prices.length()-1
+   
+        min = prices[i] < min ? prices[i] : min;
+        max = prices[i] - min > max ? prices[i] - min : max;
         
     end
+
+    max;
+end
 
     puts max_profit([ 7, 1, 5, 3, 6, 4]);
     puts max_profit([ 7, 6, 5, 4, 3, 1]);
