e194fe32aec70bb6daf1e7e114cf5bfa7a0cd66d
chad <thechanmoon@gmail.com>
2021-10-07 15:24:08 -0500

    leetcode 121 js

--- leetcode/121/MaxProfit.js
+++ leetcode/121/MaxProfit.js
@@ -72,34 +72,67 @@
 //     return x > y ? x : y;
 // }
 
-const CHAR_BIT = 4; 
-const INT_BIT = 8; 
+// const CHAR_BIT = 4; 
+// const INT_BIT = 8; 
 
-var min = function(x, y) {
-    return y + ((x - y) & ((x - y) >>  (INT_BIT * CHAR_BIT - 1)));
-}
+// var min = function(x, y) {
+//     return y + ((x - y) & ((x - y) >>  (INT_BIT * CHAR_BIT - 1)));
+// }
 
-var max = function(x, y) {
-    return x - ((x - y) & ((x - y) >> (INT_BIT * CHAR_BIT - 1)));
-}
+// var max = function(x, y) {
+//     return x - ((x - y) & ((x - y) >> (INT_BIT * CHAR_BIT - 1)));
+// }
+
+// var maxProfit = function(prices) {
+//     if(prices.length==0) {
+//         return 0;
+//     }
+//     let min_price=prices[0];
+//     let maxprofit=0;
+//     for(let i=0;i<prices.length;i++) {
+//         // maxprofit = Math.max(maxprofit,prices[i]-min_price);
+//         // min_price =  Math.min(min_price,prices[i]);
+//         // maxprofit = (maxprofit > prices[i]-min_price)? maxprofit : prices[i]-min_price;
+//         // min_price =  (min_price < prices[i]) ? min_price : prices[i];
+//         min_price =  min(min_price,prices[i]);
+//         maxprofit = max(maxprofit,prices[i]-min_price);
+//     }
+    
+//     return maxprofit;        
+// }
 
 var maxProfit = function(prices) {
-    if(prices.length==0) {
-        return 0;
-    }
-    let min_price=prices[0];
-    let maxprofit=0;
-    for(let i=0;i<prices.length;i++) {
-        // maxprofit = Math.max(maxprofit,prices[i]-min_price);
-        // min_price =  Math.min(min_price,prices[i]);
-        // maxprofit = (maxprofit > prices[i]-min_price)? maxprofit : prices[i]-min_price;
-        // min_price =  (min_price < prices[i]) ? min_price : prices[i];
-        min_price =  min(min_price,prices[i]);
-        maxprofit = max(maxprofit,prices[i]-min_price);
-    }
-    
-    return maxprofit;        
-}
+        let min = prices[0];
+        let max = 0;
+        
+        for(let i = 1; i < prices.length; i++)
+        { 
+            // console.log("day " + (i+1));
+            // console.log("before");
+            // console.log("prices[i] = "+prices[i]);
+            // console.log("min = "+min);
+            // console.log("max = "+max);
+            
+            // if(min>prices[i])
+            // {
+            //     min = prices[i];    
+            // }
+            // else
+            // {
+            //     if(prices[i] - min > max)
+            //         max = prices[i] - min;
+            // }
+            
+            min = ( min < prices[i] ) ? min : prices[i];
+            max = (prices[i] - min > max) ? prices[i] - min : max;
+            
+            // console.log("after");
+            // console.log("min = "+min);
+            // console.log("max = "+max);
+            // console.log();
+        }
+        return max;
+};
 
 console.log(maxProfit([ 7, 1, 5, 3, 6, 4]));
 console.log(maxProfit([ 7, 6, 5, 4, 3, 1]));
