46a58773bb6ee810c2c81ef08a313b0ae2b18967
chad <thechanmoon@gmail.com>
2022-01-25 10:07:05 -0600

    leetcode 1 ruby

---
+++ leetcode/1/TwoSum.rb
@@ -0,0 +1,15 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+    hashmap = {}
+
+    # array.each_with_index {|val, index| puts "#{val} => #{index}" }
+    nums.each_with_index { |value, index|           
+        if hashmap[target - value]
+            return hashmap[target - value], index
+        end
+        hashmap[value] = index 
+    }
+end
+puts(two_sum([2,7,11,15],9));
