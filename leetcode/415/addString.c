fa2a01318f18df52123f8a46d71258bf7b5d7a10
chad <thechanmoon@gmail.com>
2022-02-02 12:01:32 -0600

    leetcode 415 c

--- leetcode/415/addString.c
+++ leetcode/415/addString.c
@@ -2,22 +2,45 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+// char *addStrings(char *num1, char *num2)
+// {
+//     int i = strlen(num1) - 1, j = strlen(num2) - 1, carry = 0, digit = 0;
+//     int ch_num1 = 0, ch_num2 = 0, sum = 0;
+//     int ret_size = (strlen(num1) > strlen(num2) ? strlen(num1) + 1 : strlen(num2) + 1) + 1; //+1 for overflow, +1 for '\0'
+//     int ret_idx = ret_size - 1;
+//     char *result = (char *)malloc(sizeof(char) * (ret_size));
+
+//     result[ret_idx--] = '\0';
+//     while (i >= 0 || j >= 0 || carry > 0)
+//     {
+//         ch_num1 = i >= 0 ? num1[i--] - '0' : 0;
+//         ch_num2 = j >= 0 ? num2[j--] - '0' : 0;
+//         sum = ch_num1 + ch_num2 + carry;
+//         result[ret_idx--] = (sum % 10) + '0';
+//         carry = (int)(sum / 10);
+//         digit++;
+//     }
+//     if (digit > strlen(num1) && digit > strlen(num2))
+//         return result;
+//     else
+//         return result + 1;
+// }
+
 char *addStrings(char *num1, char *num2)
 {
     int i = strlen(num1) - 1, j = strlen(num2) - 1, carry = 0, digit = 0;
-    int ch_num1 = 0, ch_num2 = 0, sum = 0;
+    int sum = 0;
     int ret_size = (strlen(num1) > strlen(num2) ? strlen(num1) + 1 : strlen(num2) + 1) + 1; //+1 for overflow, +1 for '\0'
     int ret_idx = ret_size - 1;
     char *result = (char *)malloc(sizeof(char) * (ret_size));
 
     result[ret_idx--] = '\0';
-    while (i >= 0 || j >= 0 || carry > 0)
+    while (i >= 0 || j >= 0 || sum > 0)
     {
-        ch_num1 = i >= 0 ? num1[i--] - '0' : 0;
-        ch_num2 = j >= 0 ? num2[j--] - '0' : 0;
-        sum = ch_num1 + ch_num2 + carry;
+        sum += i >= 0 ? num1[i--] - '0' : 0;
+        sum += j >= 0 ? num2[j--] - '0' : 0;
         result[ret_idx--] = (sum % 10) + '0';
-        carry = (int)(sum / 10);
+        sum = (int)(sum / 10);
         digit++;
     }
     if (digit > strlen(num1) && digit > strlen(num2))
