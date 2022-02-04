8ac5ccdb34b8ab6ece9a9a36691fea744fd9e93a
chad <thechanmoon@gmail.com>
2022-02-04 13:49:02 -0600

    leetcode 415 make it better

--- leetcode/415/addString.c
+++ leetcode/415/addString.c
@@ -26,27 +26,55 @@
 //         return result + 1;
 // }
 
+// char *addStrings(char *num1, char *num2)
+// {
+//     int i = strlen(num1) - 1, j = strlen(num2) - 1, sum = 0, digit = 0;
+//     int ret_size = (strlen(num1) > strlen(num2) ? strlen(num1) + 1 : strlen(num2) + 1) + 1; //+1 for overflow, +1 for '\0'
+//     int ret_idx = ret_size - 1;
+//     char *result = (char *)malloc(sizeof(char) * (ret_size));
+
+//     result[ret_idx--] = '\0';
+//     while (i >= 0 || j >= 0 || sum > 0)
+//     {
+//         sum += i >= 0 ? num1[i--] - '0' : 0;
+//         sum += j >= 0 ? num2[j--] - '0' : 0;
+//         printf("%d\n", (sum % 10));
+//         result[ret_idx--] = (sum % 10) + '0';
+//         sum = (int)(sum / 10); //carry
+//         digit++;
+
+//         printf("%s\n", result);
+//         printf("===========\n");
+//     }
+//     if (digit > strlen(num1) && digit > strlen(num2))
+//         return result;
+//     else
+//         return result + 1;
+// }
+
 char *addStrings(char *num1, char *num2)
 {
-    int i = strlen(num1) - 1, j = strlen(num2) - 1, carry = 0, digit = 0;
-    int sum = 0;
-    int ret_size = (strlen(num1) > strlen(num2) ? strlen(num1) + 1 : strlen(num2) + 1) + 1; //+1 for overflow, +1 for '\0'
-    int ret_idx = ret_size - 1;
-    char *result = (char *)malloc(sizeof(char) * (ret_size));
+    int i = strlen(num1) - 1, j = strlen(num2) - 1, sum = 0, digit = 0;
+    int ret_len = (strlen(num1) > strlen(num2) ? strlen(num1) + 1 : strlen(num2) + 1) + 1; // +1 for carry, +1 for '\0';
+    int idx = ret_len - 1;
+    char *ret = (char *)malloc(sizeof(char) * ret_len);
+    //char *ret = malloc(sizeof(char) * ret_len);
+    ret[idx--] = '\0';
 
-    result[ret_idx--] = '\0';
     while (i >= 0 || j >= 0 || sum > 0)
     {
-        sum += i >= 0 ? num1[i--] - '0' : 0;
-        sum += j >= 0 ? num2[j--] - '0' : 0;
-        result[ret_idx--] = (sum % 10) + '0';
-        sum = (int)(sum / 10);
+        sum += i >= 0 ? (num1[i--] - '0') : 0;
+        sum += j >= 0 ? (num2[j--] - '0') : 0;
+        ret[idx--] = (sum % 10) + '0';
+        sum = (int)(sum / 10); //carry
+        //sum = sum / 10; //carry
         digit++;
     }
+
     if (digit > strlen(num1) && digit > strlen(num2))
-        return result;
+        return ret;
     else
-        return result + 1;
+        return ret + 1;
 }
 
 int main()
