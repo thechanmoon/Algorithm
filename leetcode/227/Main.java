7d76d0ae6f4c745edccbd497da71b2f8cd30c421
chad <thechanmoon@gmail.com>
2022-02-18 16:59:53 -0600

    leetcode 227 test case  add

--- leetcode/227/Main.java
+++ leetcode/227/Main.java
@@ -1,53 +1,56 @@
 class Main {
     public static void main(String[] args) {
         // Solution sl = new Solution();
-        // System.out.println(sl.calculate("3+2*2"));  
-        
-        Solution1 sl_1=new Solution1();
-        System.out.println(sl_1.calculate("3+2*2"));
+        // // System.out.println(sl.calculate("3+2*2"));
+
+        // Solution1 sl_1 = new Solution1();
+        // System.out.println(sl_1.calculate("3+2*2"));
+
+        Solution1 sl_1 = new Solution1();
+        System.out.println(sl_1.calculate("4+3*2"));
     }
 }
 
 // class Solution {
-//     public int calculate(String s) {
+// public int calculate(String s) {
 
-            
-//         if (s == null || s.isEmpty()) return 0;
-//         int len = s.length();
-//         Stack<Integer> stack = new Stack<Integer>();
-//         int currentNumber = 0;
-//         char operation = '+';
-//         for (int i = 0; i < len; i++) {
-//             char currentChar = s.charAt(i);
-//             if (Character.isDigit(currentChar)) {
-//                 currentNumber = (currentNumber * 10) + (currentChar - '0');
-//             }
-//             if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == len - 1) {
-//                 if (operation == '-') {
-//                   }
-//                 else if (operation == '+') {
-//                   }
-//                 else if (operation == '*') {
-//                   }
-//                 else if (operation == '/') {
-//                     stack.push(stack.pop() / currentNumber);
-//                 }
-//                 operation = currentChar;
-//                 currentNumber = 0;
-//             }
-//         }
-//         int result = 0;
-//         while (!stack.isEmpty()) {
-//             result += stack.pop();
-//         }
-//         return result;
-//     }
+// if (s == null || s.isEmpty()) return 0;
+// int len = s.length();
+// Stack<Integer> stack = new Stack<Integer>();
+// int currentNumber = 0;
+// char operation = '+';
+// for (int i = 0; i < len; i++) {
+// char currentChar = s.charAt(i);
+// if (Character.isDigit(currentChar)) {
+// currentNumber = (currentNumber * 10) + (currentChar - '0');
 // }
-
+// if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar)
+// || i == len - 1) {
+// if (operation == '-') {
+// }
+// else if (operation == '+') {
+// }
+// else if (operation == '*') {
+// }
+// else if (operation == '/') {
+// stack.push(stack.pop() / currentNumber);
+// }
+// operation = currentChar;
+// currentNumber = 0;
+// }
+// }
+// int result = 0;
+// while (!stack.isEmpty()) {
+// result += stack.pop();
+// }
+// return result;
+// }
+// }
 
 class Solution1 {
     public int calculate(String s) {
-        if (s == null || s.isEmpty()) return 0;
+        if (s == null || s.isEmpty())
+            return 0;
         int length = s.length();
         int currentNumber = 0, lastNumber = 0, result = 0;
         char operation = '+';
@@ -56,6 +59,18 @@ class Solution1 {
             if (Character.isDigit(currentChar)) {
                 currentNumber = (currentNumber * 10) + (currentChar - '0');
             }
+
+            // if(i == length - 1)
+            {
+                System.out.println("before =============");
+                System.out.println("i = " + i);
+                System.out.println("char = " + currentChar);
+                System.out.println("currentNumber = " + currentNumber);
+                System.out.println("lastNumber = " + lastNumber);
+                System.out.println("operation = " + operation);
+                System.out.println("result = " + result);
+            }
+
             if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == length - 1) {
                 if (operation == '+' || operation == '-') {
                     result += lastNumber;
@@ -67,6 +82,16 @@ class Solution1 {
                 }
                 operation = currentChar;
                 currentNumber = 0;
+            }
+
+            {
+                System.out.println("after =============");
+                System.out.println("char = " + currentChar);
+                System.out.println("currentNumber = " + currentNumber);
+                System.out.println("lastNumber = " + lastNumber);
+                System.out.println("operation = " + operation);
+                System.out.println("result = " + result);
+                System.out.println("==============");
             }
         }
         result += lastNumber;
