488f6447090756c9226a0c97961a8cf3b1f5ef23
chad <thechanmoon@gmail.com>
2022-03-03 15:19:54 -0600

    leetcode 26 python

--- leetcode/26/RemoveDuplicates.js
+++ leetcode/26/RemoveDuplicates.js
@@ -1,14 +1,14 @@
-// var removeDuplicates = function(nums) {
-//     if (nums.length == 0 || nums.length == 1)
-//         return nums.length;
-//     let index = 0;
-//     for (let i = 1; i < nums.length; i++) {
-//         if (nums[i - 1] != nums[i])
-//             nums[++index] = nums[i];
-//         console.log(nums);    
-//     }
-//     return index + 1;
-// };
+var removeDuplicates = function(nums) {
+    if (nums.length == 0 || nums.length == 1)
+        return nums.length;
+    let index = 0;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i - 1] != nums[i])
+            nums[++index] = nums[i];
+        // console.log(nums);    
+    }
+    return index + 1;
+};
 
 /* 
 i = 1 , index 0 -> 0 , nums[ 0, 0, 1, 2 ] -> nums[ 0, 0, 1, 2 ]
