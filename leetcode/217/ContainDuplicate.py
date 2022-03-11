faf5b83b654399154c7393f93a9e6bda53599034
chad <thechanmoon@gmail.com>
2022-03-11 11:32:55 -0600

    leetcode 217 python

---
+++ leetcode/217/ContainDuplicate.py
@@ -0,0 +1,29 @@
+from typing import List
+# class Solution(object):
+#     def containsDuplicate(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: bool
+#         """
+#         hash = {}
+#         for i, num in enumerate(nums):
+#             if nums[i] in hash:
+#                 return True
+#             else:
+#                 hash[nums[i]] = True
+#         return False
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hash = {}
+        for i, num in enumerate(nums):
+            if num in hash:
+                return True
+            else:
+                hash[num] = True
+        return False
+
+
+sl = Solution()
+print(sl.containsDuplicate([1, 2, 3, 1]))
