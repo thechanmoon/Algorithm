a47a05229877a48a95835e98f387976696096e70
chad <thechanmoon@gmail.com>
2022-04-06 13:49:41 -0500

    leetcode 121 python

--- leetcode/121/MaxProfit.java
+++ leetcode/121/MaxProfit.java
@@ -1,8 +1,8 @@
 class MaxProfit {
     public static void main(String[] args) {
         Solution sl = new Solution();
-        System.out.println(sl.maxProfit(new int[] { 7, 3, 5, 1, 5, 4 }));
-        // System.out.println(sl.maxProfit(new int[] { 7, 6, 5, 4, 3, 1 }));
+        // System.out.println(sl.maxProfit(new int[] { 7, 3, 5, 1, 5, 4 }));
+        System.out.println(sl.maxProfit(new int[] { 7,1,5,3,6,4 }));
     }
 }
 
@@ -132,7 +132,7 @@ class Solution {
         for(int i = 0; i < prices.length; i++){
             min_price = Math.min(min_price, prices[i]);
             max_profit = Math.max(max_profit, prices[i]-min_price);
-            System.out.println(prices[i] + ", " + min_price + ", "+ (prices[i] - min_pric) +", "  + max_profit);
+            System.out.println(prices[i] + ", " + min_price + ", "+ (prices[i] - min_price) +", "  + max_profit);
         }
         
         return max_profit;
