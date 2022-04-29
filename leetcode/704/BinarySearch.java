3476253ccf030fb090d20a639a2d9871eef84d8a
chad <thechanmoon@gmail.com>
2022-04-29 11:41:33 -0500

    leetcode 704 java make it better

--- leetcode/704/BinarySearch.java
+++ leetcode/704/BinarySearch.java
@@ -4,26 +4,48 @@ class BinarySearch {
     }
 }
 
+// class Solution {
+//     public int search(int[] nums, int target) {
+
+//         if (nums.length == 0)
+//             return -1;
+
+//         int left = 0;
+//         int right = nums.length - 1;
+
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+//             if (nums[mid] == target) {
+//                 return mid;
+//             } else if (nums[mid] > target) {
+//                 right = mid - 1;
+//             } else {
+//                 left = mid + 1;
+//             }
+//         }
+
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int search(int[] nums, int target) {
-
-        if (nums.length == 0)
-            return -1;
-
         int left = 0;
         int right = nums.length - 1;
-
+        int mid = 0;
+        int ret = -1;
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            // mid = left + (right - left) / 2;
+            mid = (left + right) / 2;
             if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else {
+                ret = mid;
+                break;
+            } else if (nums[mid] <= target) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-
-        return -1;
+        return ret;
     }
 }
