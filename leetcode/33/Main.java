bb5216f78aab3750b1644a2733a211e93771e5be
Osx <osx@Osxs-Mac-mini.local>
2022-12-05 00:41:30 -0500

    leetcode 33 java

--- leetcode/33/Main.java
+++ leetcode/33/Main.java
@@ -1,5 +1,3 @@
-import javax.lang.model.util.ElementScanner14;
-
 class Main{
     public static void main(String[] args) {
         Solution sl = new Solution();
@@ -41,6 +39,29 @@ class Solution{
             }
         }
 
+        return -1;
+    }
+
+    public int search1(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, mid;
+        while (left <= right) {
+            mid = (right - left) / 2 + left;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                if (nums[mid] > nums[right] && nums[left] > target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (nums[mid] < nums[left] && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
         return -1;
     }
 }
