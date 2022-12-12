2079d7c00bfa96590bbce82c4e4a4769e1f651cd
Osx <osx@Osxs-Mac-mini.local>
2022-12-12 06:45:18 -0500

    leetcode java 5

--- leetcode/5/LongestPalindromicSubstring.java
+++ leetcode/5/LongestPalindromicSubstring.java
@@ -1,14 +1,53 @@
+// import java.lang.*;
 
 class LongestPalindromicSubstring {
     public static void main(String[] args) {
         Solution sl = new Solution();
         // System.out.println(sl.longestPalindrome("babad"));
-        System.out.println(sl.longestPalindrome("hackerrekcahba"));
+        //System.out.println(sl.longestPalindrome("hackerrekcahba"));
+        System.out.println(sl.longestPalindrome("aba"));
 
     }
 }
 
 class Solution {
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() < 1)
+            return "";
+        int start = 0;
+        int end = 0;
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            int len1 = expandFromMiddle(s, i, i);
+            int len2 = expandFromMiddle(s, i, i+1);
+            int len = Math.max(len1, len2);
+            System.out.println("before =====");
+            System.out.println("i = " + i + " len = " + len + " start = " + start + " end " + end);
+            if(len > end - start){
+                start = i - ((len -1)/2);
+                end = i + ((len)/2);
+            }  
+            System.out.println("after =====");
+            System.out.println("i = " + i + " len = " + len + " start = " + start + " end " + end);          
+        }
+        return s.substring(start, end +1);
+    }
+
+    public int expandFromMiddle(String s, int left, int right){
+        if(s == null || right > right) return 0;
+
+        while(left >= 0 && right < s.length() && s.charAt(left--)== s.charAt(right++))
+        {
+            // left--;
+            // right++;
+        }
+
+        return right -left -1;
+    }
+}
+
+class Solution1  {
     public String longestPalindrome(String s) {
         int max = 0;
         String retVal = "";
