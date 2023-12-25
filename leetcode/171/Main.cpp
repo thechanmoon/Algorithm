3188f82eca3ba28f28a0aa5e9c77511232e5b705
Chan Park <thechanmoon@gmail.com>
2023-12-25 10:40:37 -0500

    leetcode 171 c++

---
+++ leetcode/171/Main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+class Solution
+{
+public:
+    int titleToNumber(string columnTitle)
+    {
+        int num = 0;
+        int sum = 0;
+        for (char c : columnTitle)
+        {
+            num = c - 'A' + 1;
+            sum = sum * 26 + num;
+        }
+        return sum;
+    }
+};
+
+int main()
+{
+    Solution sl;
+
+    vector<int> result;
+    result.push_back(sl.titleToNumber("A"));
+    result.push_back(sl.titleToNumber("AB"));
+    result.push_back(sl.titleToNumber("ZY"));
+
+    for (unsigned i = 0; i < result.size(); i++)
+        cout << ' ' << result.at(i) << "\n";
+    cout << "\n";
+    return 0;
+};
+
+// gcc -o Main Main.cpp -lstdc++
+// g++ -std=c++17 -o Main Main.cpp
