59b54c9a5d7612db8389be034bce2768dbd56152
Chan Park <thechanmoon@gmail.com>
2024-05-03 03:42:24 -0400

    leetcode 442 python

--- leetcode/442/FindDuplicate.py
+++ leetcode/442/FindDuplicate.py
@@ -1,21 +1,33 @@
-class Solution(object):
-    def findDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+# class Solution(object):
+#     def findDuplicates(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[int]
+#         """
         
-        ret = []
+#         ret = []
     
-        for num in nums:    
+#         for num in nums:    
             
-            if nums[abs(num)-1] < 0:
-                ret.append(abs(num))
-            else:
-                nums[abs(num)-1] = -nums[abs(num)-1]   
+#             if nums[abs(num)-1] < 0:
+#                 ret.append(abs(num))
+#             else:
+#                 nums[abs(num)-1] = -nums[abs(num)-1]   
 
-            # print(nums, ret)
-        return ret
-    
+#             # print(nums, ret)
+#         return ret
+
+from typing import List
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        ret = []
+        for n in nums:
+            n = abs(n)
+            if nums[n-1]< 0:
+                ret.append(n)
+            nums[n-1] = -nums[n-1]
+        return ret     
+
 sl = Solution();
 print(sl.findDuplicates([4,3,2,7,8,2,3,1]));    
