ab7f82a9621a10e2dc307068a200ff4db561d9ac
Chan Park <thechanmoon@gmail.com>
2024-05-11 23:09:11 -0400

    leetcode 217 javascript

--- leetcode/217/ContainDuplicate.js
+++ leetcode/217/ContainDuplicate.js
@@ -1,15 +1,55 @@
-var containsDuplicate = function(nums) {
-        
-    let set = new Set();
+// var containsDuplicate = function(nums) {
 
-    for(let i = 0; i < nums.length; i++)
-    {
-        if(set.has(nums[i]))
-            return true;
-        else
-            set.add(nums[i]);
+//     let set = new Set();
+
+//     for(let i = 0; i < nums.length; i++)
+//     {
+//         if(set.has(nums[i]))
+//             return true;
+//         else
+//             set.add(nums[i]);
+//     }
+//     return false;
+// };
+
+// var containsDuplicate = function (nums) {
+//     let set = new Set();
+//     let ret = false;
+//     for (let i = 0; i < nums.length; i++) {
+//         if (set.has(nums[i])) {
+//             ret = true;
+//             break;
+//         } else set.add(nums[i]);
+//     }
+//     return ret;
+// };
+
+var containsDuplicate = function (nums) {
+    let set = new Set();
+    let ret = false;
+    console.log(nums);
+    for (let num of nums) {
+        console.log(num, set);
+        if (set.has(num)) {
+            ret = true;
+            break;
+        } else set.add(num);
     }
-    return false;
+    return ret;
 };
 
-console.log(containsDuplicate([1,2,3,1]));
+//for of, in loop test
+// var forloop = (numbers) => {
+//     for (let x of numbers) {
+//         console.log(x);
+//     }
+
+//     let txt = "";
+//     for (let x in numbers) {
+//         txt += numbers[x] + ",";
+//     }
+//     console.log(txt);
+// };
+// forloop([1, 2, 3, 1]);
+
+console.log(containsDuplicate([1, 2, 3, 1]));
