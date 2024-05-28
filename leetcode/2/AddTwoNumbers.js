c97034c00bac0371f8827ce094a716aae988be9b
Chan Park <thechanmoon@gmail.com>
2024-05-28 00:35:43 -0400

    leetcode 2 javascript

--- leetcode/2/AddTwoNumbers.js
+++ leetcode/2/AddTwoNumbers.js
@@ -10,7 +10,13 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
- var addTwoNumbers = function(l1, l2) {
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+     
+var addTwoNumbers = function(l1, l2) {
     let retList = new ListNode(0);
     let curList = retList;
     let carry = 0;
@@ -32,3 +38,17 @@
     
     return retList.next;
 };
+
+let l1 = new ListNode(2);
+l1.next = new ListNode(4);
+l1.next.next = new ListNode(3);
+let l2 = new ListNode(5);
+l2.next = new ListNode(6);
+l2.next.next = new ListNode(4);
+//System.out.println(sl.addTwoNumbers(l1, l2));
+let ret = addTwoNumbers(l1, l2);
+while(ret != null)
+{
+    console.log(ret.val);
+    ret = ret.next;
+} 
