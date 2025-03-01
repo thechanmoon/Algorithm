da76942e7146546bc879dce0ac889848213ffc26
Chan Park <thechanmoon@gmail.com>
2025-03-01 07:36:38 -0500

    make it better

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -15,7 +15,41 @@ class LinkedList{
         this.tail = node;
         this.length = 1;
     }
+
+    push (data)
+    {
+        const node = new Node(data)
+        if(!this.head){
+            this.head = node;
+            this.tail = node;
+        }else{
+            this.tail.next = node;
+            this.tail=node;
+        }
+        this.length++;
+        return this;
+    }
+
+    print()
+    {
+
+        let curretNode = this.head;
+        // sconsole.log(node);
+        while(node)
+        {
+            console.log(node);
+            curretNode = node.next;
+            // console.log(node);
+            node = nodeNext;
+        }
+    }
 }
 
 const list = new LinkedList(4);
 console.log(list);
+console.log("0 ==============")
+list.push(7);
+console.log(list);
+console.log("1 ==============")
+list.print();
+console.log("2 ==============")
