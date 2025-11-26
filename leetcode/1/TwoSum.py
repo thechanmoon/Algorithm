02e3d83ff94010d6933d12c67e9b45b8dc7c1d92
thechanmoon <40532728+thechanmoon@users.noreply.github.com>
2025-11-27 06:38:11 +0900

    Make it better

--- leetcode/1/TwoSum.py
+++ leetcode/1/TwoSum.py
@@ -13,5 +13,17 @@ class Solution(object):
             
             hashmap[value] = i 
 
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         for i in range((len(nums))):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i,j]
+
 sl = Solution();
 print(sl.twoSum([2,7,11,15],9));
