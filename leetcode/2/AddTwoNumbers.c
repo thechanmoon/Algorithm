f04b661f999c5cf53a01719e7487dd87a6e2a3a5
Chan Park <thechanmoon@gmail.com>
2026-01-17 17:34:11 -0500

    leecode 2 c make it better

--- leetcode/2/AddTwoNumbers.c
+++ leetcode/2/AddTwoNumbers.c
@@ -12,44 +12,65 @@ struct ListNode {
       struct ListNode *next;
 };
  
-struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-    struct ListNode *ret = malloc(sizeof(struct ListNode));
-    struct ListNode *cur = ret;
-    int carry = 0;
-    int sum = 0;
-    while(l1 || l2 || carry){
+// struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+//     struct ListNode *ret = malloc(sizeof(struct ListNode));
+//     struct ListNode *cur = ret;
+//     int carry = 0;
+//     int sum = 0;
+//     while(l1 || l2 || carry){
 
-        sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
-        carry = sum/10;
+//         sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+//         carry = sum/10;
         
-        cur->next = malloc(sizeof(struct ListNode));
-        cur->next->val = sum%10;
-        cur->next->next = NULL;
-        cur = cur->next;
+//         cur->next = malloc(sizeof(struct ListNode));
+//         cur->next->val = sum%10;
+//         cur->next->next = NULL;
+//         cur = cur->next;
 
+//         if(l1)
+//             l1 = l1->next;
+//         if(l2)
+//             l2 = l2->next;    
+//     }
+//     return ret->next;
+// }
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummyHead;
+    struct ListNode* current = &dummyHead;
+    int carry = 0;
+    
+
+    while (l1 || l2 || carry > 0)
+    {
+        int sum = (l1 ? l1->val : 0) + (l2? l2->val : 0) + carry;
+        carry = sum / 10;
+        current->next = (struct ListNode*)malloc(sizeof (struct ListNode));
+        current->next->val = sum%10;
+        current->next->next = NULL;    
         if(l1)
             l1 = l1->next;
         if(l2)
-            l2 = l2->next;    
+            l2 = l2->next;
+
+        current = current->next;        
     }
-    return ret->next;
+    return dummyHead.next;
 }
-
-
 int main()
 {
-    struct ListNode* l1 = malloc(sizeof(struct ListNode));
+    struct ListNode* l1 = (struct ListNode*)malloc(sizeof(struct ListNode));
     l1->val = 2;
-    l1->next = malloc(sizeof(struct ListNode));
+    l1->next =(struct ListNode*)malloc(sizeof(struct ListNode));
     l1->next->val = 4;
-    l1->next->next = malloc(sizeof(struct ListNode));
+    l1->next->next = (struct ListNode*)malloc(sizeof(struct ListNode));
     l1->next->next->val = 3;
-    struct ListNode* l2 = malloc(sizeof(struct ListNode));
+    struct ListNode* l2 = (struct ListNode*)malloc(sizeof(struct ListNode));
 
     l2->val = 5;
-    l2->next = malloc(sizeof(struct ListNode));
+    l2->next = (struct ListNode*)malloc(sizeof(struct ListNode));
     l2->next->val = 6;
-    l2->next->next = malloc(sizeof(struct ListNode));
+    l2->next->next = (struct ListNode*)malloc(sizeof(struct ListNode));
     l2->next->next->val = 4;
 
     struct ListNode* ret = addTwoNumbers(l1, l2);
